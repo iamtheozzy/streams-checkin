c06abcbe862919dc2f00a43ee1ad8340fa9dd8f3
Ozzy <ocardenas789@me.com>
2022-09-30 21:09:39 -0500

    add chakra ui

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import type { DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import { customTheme } from "../styles/theme";
+
+class MyDocument extends Document {
+  static getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <ColorModeScript
+            initialColorMode={customTheme.config?.initialColorMode}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
